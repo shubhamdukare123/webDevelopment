4ab4c95bc0ea5dd9f6778ee57d5862397d86c1a9
shubhamdukare123 <109910924+shubhamdukare123@users.noreply.github.com>
2023-08-04 00:09:34 +0530

    javascript

--- javaScript/app.js
+++ javaScript/app.js
@@ -4,3 +4,14 @@ let a=10;
 let b=5;
 console.log("sum is",a+b);
 //this is comment
+let output = `The total price is: ${a+b} Rupees.`;
+console.log(`The total price is: ${a+b} Rupees.`);
+alert("Designed by Shubham");
+console.error("This is error msg");
+let why = prompt("Why are  you so beautiful?");
+alert(why);
+let str = "Shubham      ";
+console.log(str.trim());
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.indexOf("h"));
---
+++ javaScript/practice1.js
@@ -0,0 +1,12 @@
+let msg = "Hello      "
+// newMsg = msg.trim();
+// console.log(newMsg);
+// newMsg= newMsg.toUpperCase();
+// console.log(newMsg);
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
+let str = "ILOVECODING";
+console.log(str.replace("LOVE", "Do")); //11-2 => 9
+console.log("Repeated : " , str.repeat(3));
+let msg1 = "apnaapnaapna"
+console.log(msg1.replace("apna","Shubham"))
