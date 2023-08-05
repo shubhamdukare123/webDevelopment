7d1f7e302f4f7dbc73fb51e5b8f68082858fff92
shubhamdukare123 <109910924+shubhamdukare123@users.noreply.github.com>
2023-08-05 11:42:13 +0530

    javascript

--- javaScript/practice3.js
+++ javaScript/practice3.js
@@ -1,5 +1,14 @@
-for(let i=1; i<=10; i++){
-    if(i%2!=0){
-        console.log(i);
-    } 
+// for(let i=1; i<=10; i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     } 
+// }
+// let favouriteMovie = "Avatar";
+// let guess = prompt("");
+// while(guess==favouriteMovie){
+//     console.log("Congratulations you guessed right!");
+// }
+fruits = ["mango", "apple", "banana", "grapes"];
+for(fruit of fruits){
+    console.log(fruit);
 }
