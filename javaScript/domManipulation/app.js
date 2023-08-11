85c1dc64e58765e743e5c9c041c8f881c2baf3e9
shubhamdukare123 <109910924+shubhamdukare123@users.noreply.github.com>
2023-08-12 01:17:58 +0530

    DOM

--- javaScript/domManipulation/app.js
+++ javaScript/domManipulation/app.js
@@ -6,5 +6,19 @@
 // //     console.dir(imgClass[i].src);
 // // }
 
-console.log(document.querySelector("#heading1"));
-console.dir(document.querySelectorAll("p"));
+// console.log(document.querySelector("#heading1"));
+// console.dir(document.querySelectorAll("p"));
+
+// let img = document.querySelector("img");
+// let h = document.querySelector("#heading1");
+// let para = document.querySelectorAll(".para");
+// let paraEl = null;
+// for(paraEl of para){
+//     paraEl.style.color = "red";
+//     paraEl.style.fontSize = "2em";
+// }
+
+let para = document.querySelector(".para");
+let div = document.querySelector("#divId");
+let body = document.querySelector("body");
+
