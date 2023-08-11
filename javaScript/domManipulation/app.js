66e93b878fc1f25be614a27cd30055961f2686a6
shubhamdukare123 <109910924+shubhamdukare123@users.noreply.github.com>
2023-08-11 23:28:27 +0530

    js

---
+++ javaScript/domManipulation/app.js
@@ -0,0 +1,10 @@
+// let natImg = console.dir(document.getElementsByClassName("natureImg"));
+// console.log(natImg);
+// // console.log(natImg.length);
+
+// // for(let i=0; i<imgClass.length; i++){
+// //     console.dir(imgClass[i].src);
+// // }
+
+console.log(document.querySelector("#heading1"));
+console.dir(document.querySelectorAll("p"));
--- javaScript/forEach.js
+++ javaScript/forEach.js
@@ -1,42 +1,52 @@
 
-let arr = [10, 20, 30,40];
+// let arr = [10, 20, 30,40];
 
-arr.forEach(function (el){
-    console.log(el);
-});
-// let print = function (el){
+// arr.forEach(function (el){
 //     console.log(el);
-// }
+// });
+// // let print = function (el){
+// //     console.log(el);
+// // }
  
-// arr.forEach(print);
+// // arr.forEach(print);
 
-let arr1 = [
-    {
-        name : "Shubham",
-        marks : 8.16
-    },
-    {
-        name : "Himanshu",
-        marks : 8.27
-    }, 
-    {
-        name : "Rohit",
-        marks : 6.50
-    } 
-]
-arr1.forEach((friend) => {
-    console.log(friend.marks);
-});
+// let arr1 = [
+//     {
+//         name : "Shubham",
+//         marks : 8.16
+//     },
+//     {
+//         name : "Himanshu",
+//         marks : 8.27
+//     }, 
+//     {
+//         name : "Rohit",
+//         marks : 6.50
+//     } 
+// ]
+// arr1.forEach((friend) => {
+//     console.log(friend.marks);
+// });
 
-let cgpa = arr1.map((el) =>{
-    return el.marks;
-});
+// let cgpa = arr1.map((el) =>{
+//     return el.marks;
+// });
 
-let newArr = [1,2,3,4];
-newArr.filter((el) => ((el%2 == 0)));
+// let newArr = [1,2,3,4];
+// newArr.filter((el) => ((el%2 == 0)));
 
-function sum(a,b=3){
-    console.log(a);
-    console.log(b);
+// function sum(a,b=3){
+//     console.log(a);
+//     console.log(b);
+// }
+// function sum(...args){
+//     for(let i=0; i<args.length; i++){
+//     console.log("In sum" , args[i]);
+//     }
+// }
+// sum(2,2,4,5);
+
+function min(){
+    console.log(arguments);
 }
-sum(2);
+
